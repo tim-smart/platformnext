cba54e31eeace8efc2cae9fb08e5094f950a248f
Tim <hello@timsmart.co>
2024-06-20 15:01:53 +1200

    update find-my-way-ts

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -3,7 +3,7 @@ import * as Http from "@effect/platform/HttpServer"
 export const runtime = "edge"
 
 const router = Http.router.empty.pipe(
-	Http.router.get("/", Http.response.text("Hello, world!")
-))
+  Http.router.get("/", Http.response.text("Hello, world!"))
+)
  
 export const GET = Http.app.toWebHandler(router)
