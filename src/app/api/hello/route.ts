c09096e8363d5c97835ff23f88d36e675f5d0cf5
Tim <hello@timsmart.co>
2024-06-20 14:51:46 +1200

    use router

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,9 @@
 import * as Http from "@effect/platform/HttpServer"
 
 export const runtime = "edge"
+
+const router = Http.router.empty.pipe(
+	Http.router.get("/", Http.response.text("Hello, world!")
+))
  
-export const GET = Http.app.toWebHandler(Http.response.text("Hello, world!"))
+export const GET = Http.app.toWebHandler(router)
